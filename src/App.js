bc786ffe78f4b78edb57c69440ac73c8697269b2
Olatunji Omisakin <msknolatunji@gmail.com>
2021-02-07 04:24:15 +0100

    Final Touch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from "react";
+import "./App.css"
 
-const App = () => (<PersonProfile />);
+const App = () => <PersonProfile />
 
 const PersonProfile = () => {
+  const people = [
+    {
+      img: 22,
+      name: "John",
+      job: "Developer"
+    },
+    {
+      img: 48,
+      name: "Bob",
+      job: "Designer"
+    },
+    {
+      img: 22,
+      name: "peter",
+      job: "artist"
+    }
+  ]
 
   return (
     <div>
-      <Person />
+      <Person person={people[0]} />
+      <Person person={people[1]}>
+        This should had been a dummy text consisting of fifteen words to mimic a sentence.
+      </Person>
+      <Person person={people[2]} />
     </div>
-  )
+  ) 
 }
 
-const Person = () => {
+const Person = (props) => {
+  const {img, name, job} = props.person;
+  const {children} = props;
 
-  const URL = "https://randomuser.me/api/portraits/thumb/men/22.jpg"
+  const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`;
 
   return (
-    <div>
-      <img src={URL} alt="" />
+    <div className="person">
+      <img src={url} alt="" />
       <div>
-        <h4>Name</h4>
-        <h4>Job</h4>
+        <h4>{name}</h4>
+        <h4>{job}</h4>
+        {children}
       </div>
     </div>
   )
